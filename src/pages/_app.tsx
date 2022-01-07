38e47d5c9fdfd8165008af2009586bd4333789df
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-01-07 14:14:11 +0700

    chore: integrate chakra ui to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
