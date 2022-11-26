e1f8fa34663b98c67394b8eb37d43109cf15ef5b
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-26 19:44:33 +0700

    chore: setup next-seo and next-sitemap configurations

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: undefined,
+  titleTemplate: '%s | Rikumovies',
+  defaultTitle: 'Rikumovies',
+  description: 'Movie collection web app',
+  canonical: 'https://rikumovies.akmalhisyam.my.id',
+};
+
+module.exports = defaultSEOConfig;
